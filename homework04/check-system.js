3c0ad3da8a7cc5e98c0ee8697f20d500795bdded
backslash112 <yangcun@live.com>
2018-05-31 17:53:14 -0500

    clean code

--- homework04/check-system.js
+++ homework04/check-system.js
@@ -1,16 +1,16 @@
 'use strict';
 
+// Question:
 // Write a Node code with a function checkSystem() that is responsible on checking
 // if the system memory size Is at least 4 GB and the processor has more than 2 cores (use os module).
 // 1. When you call the function, you should receive an immediate message on the console "Checking your system...”.
 // 2. if the system doesn t have enough memory we should print a message to the console: "This app needs at least 4 GB of RAM’ 3.
 // 3. If the system doesnt nave at least 2 cores, print this message to the console: Processor is not supported
 // 4. It the system has enough specs, print the following message "System 1S checked successfully".
-
 // Rewrite the last question using Observables.
 
 
-// version 1:
+// Solution 1:
 const os = require('os');
 
 const checkSystem = () => {
@@ -24,17 +24,14 @@ const checkSystem = () => {
     console.log('Processor is not supported.');
     match = false;
   }
-
   if (match) {
     console.log('System is checked successfully.')
   }
 }
-
 // checkSystem();
 
 
-// version 2:
-
+// Solution 2:
 var Promise = require('promise');
 
 const checkSystem2 = () => {
@@ -55,9 +52,10 @@ const checkSystem2 = () => {
     }
   })
 }
+// checkSystem2();
 
-// version 3:
 
+// Solution 3:
 const Rx = require('rxjs');
 
 let promise =  new Promise((resolve, reject) => {
