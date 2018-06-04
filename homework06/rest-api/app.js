abb48c1786da94fb20a492b2617c600069c79652
backslash112 <yangcun@live.com>
2018-06-03 21:47:52 -0500

    enable CORS

--- homework06/rest-api/app.js
+++ homework06/rest-api/app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 // var validator = require('express-validator');
-
+const cors = require('cors');
 
 var indexRouter = require('./routes/index');
 // var usersRouter = require('./routes/users');
@@ -24,6 +24,8 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(cors());
 
 app.use('/', indexRouter);
 // app.use('/users', usersRouter);
