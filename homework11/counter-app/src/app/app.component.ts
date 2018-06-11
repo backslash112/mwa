1ae0aca4830a7007f995519b324f3504eb31dad5
backslash112 <yangcun@live.com>
2018-06-11 15:42:32 -0500

    bind/pass property to sub component

--- homework11/counter-app/src/app/app.component.ts
+++ homework11/counter-app/src/app/app.component.ts
@@ -2,15 +2,18 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: '<h3>{{ title }}</h3> \
-              <app-counter counter="10" (counterChanged)="onCounterChanged($event)"></app-counter>',
+  template: '<h3>component Counter Value = {{ componentCounterValue }}</h3> \
+              <app-counter counter="{{ componentCounterValue }}" (counterChanged)="onCounterChanged($event)"></app-counter>',
   styles: []
 })
 export class AppComponent {
-  title = 'app';
+  componentCounterValue: number;
 
+  constructor() {
+    this.componentCounterValue = 10;
+  }
   onCounterChanged(newValue) {
     console.log(newValue);
-    this.title = newValue;
+    this.componentCounterValue = newValue;
   }
 }
