34f881fc242a6f5894d9e7466443fd919c1896ad
backslash112 <yangcun@live.com>
2018-06-12 14:49:35 -0500

    add the visible directive

--- homework12/my-app/src/app/myvisibility.directive.ts
+++ homework12/my-app/src/app/myvisibility.directive.ts
@@ -1,10 +1,18 @@
-import { Directive } from '@angular/core';
+import { Directive, Input, ElementRef, Renderer } from '@angular/core';
 
 @Directive({
   selector: '[appMyvisibility]'
 })
 export class MyvisibilityDirective {
 
-  constructor() { }
+  @Input() visibility: boolean;
+  constructor(private e: ElementRef, private r: Renderer) { 
+    
+  }
+
+  ngOnInit() {
+    this.e.nativeElement.style.visibility = this.visibility ? "visible" : "hidden";
+  }
+
 
 }
